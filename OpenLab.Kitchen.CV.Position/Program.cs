9fa5d64d0fef62d49ce501ce57dec31ddbe9850f
Gerard Wilkinson <g.wilkinson@newcastle.ac.uk>
2016-11-21 10:31:29 +0000

    Finished motion detector for ground truth movement data.

--- OpenLab.Kitchen.CV.Position/Program.cs
+++ OpenLab.Kitchen.CV.Position/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenCvSharp;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System.Drawing;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
 
 namespace OpenLab.Kitchen.CV.Position
 {
@@ -13,49 +17,57 @@ namespace OpenLab.Kitchen.CV.Position
 
         static void Main(string[] args)
         {
-            var camera = new VideoCapture("capture.mp4");
-            Mat firstFrame = camera.RetrieveMat();
+            var camera = new Capture("lowcrop.mp4");
 
-            var output = new VideoWriter("test.mp4", FourCC.H264, 30, firstFrame.Size());
-            firstFrame.Resize(500);
-            output.Write(firstFrame);
+            CvInvoke.NamedWindow("test");
 
-            Mat delta = firstFrame.Clone();
-            Mat grey = new Mat();
-            Mat blur = grey.Clone();
-            Mat thresh = grey.Clone();
+            Mat firstFrameGrey = new Mat();
+            Mat firstFrame = new Mat();
+            CvInvoke.CvtColor(new Mat("Capture.PNG", LoadImageType.AnyColor), firstFrameGrey, ColorConversion.Bgr2Gray);
+            CvInvoke.GaussianBlur(firstFrameGrey, firstFrame, new Size(21, 21), 0);
 
-            using (var window = new Window("capture"))
-            using(var frame = new Mat())
+            var frame = new Mat();
+            var delta = new Mat();
+            var grey = new Mat();
+            var blur = new Mat();
+            var thresh = new Mat();
+            var dilate = new Mat();
+
+            camera.Start();
+
+            camera.ImageGrabbed += (sender, eventArgs) =>
             {
-                while (true)
+                camera.Retrieve(frame);
+                try
                 {
-                    camera.Read(frame);
-                    window.ShowImage(frame);
-                    /*frame.Resize(500);
-                    Cv2.CvtColor(frame, grey, ColorConversionCodes.BGR2GRAY);
-                    Cv2.GaussianBlur(grey, blur, new Size(21, 21), 0);
+                    CvInvoke.CvtColor(frame, grey, ColorConversion.Bgr2Gray);
+                    CvInvoke.GaussianBlur(grey, blur, new Size(21, 21), 0);
 
-                    Cv2.Absdiff(firstFrame, frame, delta);
-                    Cv2.Threshold(delta, thresh, 25, 255, ThresholdTypes.Binary);
+                    CvInvoke.AbsDiff(firstFrame, blur, delta);
+                    CvInvoke.Threshold(delta, thresh, 25, 255, ThresholdType.Binary);
 
-                    Cv2.Dilate(thresh, thresh, null, null, 2);
+                    CvInvoke.Dilate(thresh, dilate, null, Point.Empty, 0, BorderType.Default, new MCvScalar());
 
-                    Mat[] conts;
-                    Cv2.FindContours(thresh, out conts, null, RetrievalModes.External,
-                        ContourApproximationModes.ApproxSimple);
+                    VectorOfVectorOfPoint conts = new VectorOfVectorOfPoint();
+                    CvInvoke.FindContours(dilate, conts, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+                    CvInvoke.Imshow("test", delta);
 
-                    foreach (var c in conts)
+                    for (var i = 0; i < conts.Size; i++)
                     {
-                        if (Cv2.ContourArea(c) < MinArea) continue;
+                        if (CvInvoke.ContourArea(conts[i]) < MinArea) continue;
 
-                        var rect = Cv2.BoundingRect(c);
-                        Cv2.Rectangle(frame, rect, new Scalar(0, 255), 2);
-
-                        output.Write(frame);
-                    }*/
+                        var rect = CvInvoke.BoundingRectangle(conts[i]);
+                        CvInvoke.Rectangle(frame, rect, new MCvScalar(0, 255), 2);
+                        CvInvoke.Imshow("test", frame);
+                    }
                 }
-            }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            };
+
+            CvInvoke.WaitKey();
         }
     }
 }
